4a2e0ae7ac08055a9044dce2002600f2a2dd4a5b
dwerner <dan.werner@hubblehome.com>
2015-11-02 11:20:40 -0800

    merp

--- index.js
+++ index.js
@@ -5,56 +5,85 @@ const EventEmitter = require('events').EventEmitter;
 const util = require('util');
 const protobuf = require('protocol-buffers');
 const fs = require('fs');
+const messages = protobuf(fs.readFileSync('./proto/messages.proto'));
 
-const messages = protobuf(fs.readFileSync('./proto/state_response.proto'));
+class Client extends EventEmitter {
+	constructor() {
+		super();
+		this.socket = dgram.createSocket('udp4');
+		this.socket.on('error', (err) => {
+			console.log('error in socket:');
+			throw err;
+		});
+		this.socket.on('message', (msg, rinfo) => {
+			this.emit('message', deserialize(msg) );
+		});
+		this.socket.on('listening', () => {
+			this.port = this.socket.address().port;
+			this.emit('listening');
+		});
+		this.socket.bind(() => {
+			this.emit('bound');
+		});
+	}
 
+	send(message, target, port, cb) {
+		let buffer = new Buffer(serialize(message));
+		console.log('sending buffer(hex): ', buffer.toString('hex'), 0, buffer.length, port, target);
+		this.socket.send(buffer, 0, buffer.length, port, target, (err) => {
+			if (err) {
+				console.log("Encountered error sending message :", message);
+				throw err;
+			} else {
+				this.socket.close();
+				cb();
+			}
+		});
+	}
 
-function Client() {
-  this.socket = dgram.createSocket('udp4');
-	this.socket.on('error', (err) => {
-		console.log('error in socket:');
-		throw err;
-	});
-	this.socket.on('message', (msg, rinfo) => {
-		this.emit('message', deserialize(msg) );
-	});
-	this.socket.on('listening', () => {
-		this.port = this.socket.address().port;
-		this.emit('listening');
-	});
-	this.socket.bind(() => {
-		this.emit('bound');
-	});
+	close() { this.socket.close(); }
 }
 
-util.inherits(Client, EventEmitter);
-
-Client.prototype.send = function (message, target, port, cb) {
-	let buffer = new Buffer(serialize(message));
-	console.log('sending buffer: ', buffer.toString('hex'), 0, buffer.length, port, target);
-	this.socket.send(buffer, 0, buffer.length, port, target, (err) => {
-		if (err) {
-			console.log("Encountered error sending message :", message);
-			throw err;
-		} else {
-			this.socket.close();
-			cb();
-		}
-	});
-};
-
-Client.prototype.close = function () {
-	this.socket.close();
-};
+class Event {
+	constructor(options) {
+		console.log(options);
+		this.deviceId = options.deviceId || "";
+		this.peripheralId = options.peripheralId || "";
+		this.profileId = options.profileId || "";
+		this.newState = newValueState(options.newState);
+		this.oldState = newValueState(options.oldState);
+		this.timestamp = options.timestamp || 0;
+	}
+}
 
 function serialize(message) {
 	return messages.Event.encode(message);
-	//return JSON.stringify(message);
+//	return JSON.stringify(message);
 }
 
 function deserialize(message) {
-	return messages.Event.decode(message);
-	//return JSON.parse(message.toString());
+	return new Event(messages.Event.decode(message));
+//	return JSON.parse(message.toString());
 }
 
+
+function newValueState(options) {
+	let state = {};
+	console.log(options);
+	if (options.value !== undefined) {
+		let valueAccessor = isNaN(options.value) ? "string_value" : "double_value";
+		state[valueAccessor] = options.value;
+	} else {
+		if (options.string_value !== undefined) {
+			state.value = 
+				state.string_value = options.string_value;
+		} else if (options.double_value !== undefined) {
+			state.value = 
+				state.double_value = options.double_value;
+		}
+	}
+	return state;
+}
+
+Client.Event = Event;
 module.exports = Client;

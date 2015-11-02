ac5146a4d69935fb33d609478f5cd828c08e21ca
dwerner <dan.werner@hubblehome.com>
2015-11-02 12:29:02 -0800

    Log connected clients

--- server.js
+++ server.js
@@ -12,14 +12,22 @@ c.on('listening', () => {
 
 	console.log("server listening on port "+port+"...");
 	c.on('message', (msg, rinfo) => {
-		if (! clients.findIndex(x => { 
-				return x.address === rinfo.address &&
-					x.port === rinfo.port;
-		}) ) {
-			console.log("got a subscription from "+rinfo.address+":"+rinfo.port, JSON.stringify(msg,null,2));
-			clients.push({address:rinfo.address, port:rinfo.port});
+		let existingClient = clients.find(x => { 
+				return x.address === rinfo.address && x.port === rinfo.port;
+		});
+		if (! existingClient ) {
+			console.log("got a subscription from "+rinfo.address+":"+rinfo.port);
+			clients.push({address:rinfo.address, port:rinfo.port, messageCount:1});
+		} else {
+			console.log("message from existing subscription recvd.", existingClient.address, existingClient.port);
+			existingClient.messageCount += 1;
 		}
 	});
+
+	setTimeout( () => {
+		console.log("Clients connected: "+clients.length);
+		console.log(JSON.stringify(clients, null, 2));
+	}, 10000);
 
 });
 

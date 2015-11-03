a3ea4284dc663ffe582347786d50ff89f84c0514
dwerner <dan.werner@hubblehome.com>
2015-11-02 16:31:11 -0800

    Server sends to clients

--- server.js
+++ server.js
@@ -25,13 +25,14 @@ c.on('listening', () => {
 				expectedSequence:1
 			});
 		} else {
-			console.log("message from existing subscription recvd.",
-					existingClient.address, existingClient.port);
 			existingClient.messageCount += 1;
 			existingClient.bytes += length;
 			if (msg.sequence !== existingClient.expectedSequence) {
 				console.log("Messages out of sequence. Expected:"+existingClient.expectedSequence+
 						" found: "+msg.sequence);
+			} else {
+				console.log("message from existing subscription recvd.",
+						existingClient.address, existingClient.port);
 			}
 			existingClient.expectedSequence += 1;
 		}
@@ -40,6 +41,19 @@ c.on('listening', () => {
 	setInterval( () => {
 		console.log("Clients connected: "+clients.length);
 		console.log(JSON.stringify(clients, null, 2));
+
+		clients.forEach( client => {
+			console.log("Sending message to client "+address+":"+port);
+			c.send(new Client.Event({
+				newState: { value: "two" },
+				oldState: { value: "one" },
+				sequence: 0,
+				timestamp: 0
+			}), client.address, client.port, (sendProperties) => {
+				console.log(sendProperties);
+			});
+		});
+
 	}, 5000);
 
 });
